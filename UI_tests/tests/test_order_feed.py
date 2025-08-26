bd02ea0f22318e2e0b8fd74352b8b278197712fd
krezzo1209 <kiriniki999@gmail.com>
2025-08-26 17:56:59 +0300

    test_order_feed.py

--- UI_tests/tests/test_order_feed.py
+++ UI_tests/tests/test_order_feed.py
@@ -1,20 +1,26 @@
-def test_order_counters_increase(driver):
-    page = MainPage(driver)
-    page.open()
+import allure
+from pages.main_page import MainPage
 
-    # Добавляем ингредиент и оформляем заказ
-    page.click_first_ingredient()
-    page.place_order()
 
-    # Ждём закрытия модального окна
-    WebDriverWait(driver, 10).until(
-        EC.invisibility_of_element_located((By.CLASS_NAME, "order-number"))
-    )
+@allure.suite("UI: Лента заказов")
+class TestOrderFeed:
 
-    # Проверяем счётчики
-    assert page.get_total_orders() > 0
-    assert page.get_today_orders() > 0
+    @allure.title("Счётчики заказов увеличиваются после оформления заказа")
+    def test_order_counters_increase(self, driver):
+        page = MainPage(driver)
+        page.open()
 
-    # Проверяем, что заказ в работе
-    order_number = page.get_order_number()
-    assert page.is_order_in_progress(order_number)
+        with allure.step("Добавляем ингредиент и оформляем заказ"):
+            page.click_first_ingredient()
+            page.place_order()
+
+        with allure.step("Ждём закрытия модального окна заказа"):
+            page.wait_for_invisibility(page.ORDER_NUMBER)
+
+        with allure.step("Проверяем, что счётчики увеличились"):
+            assert page.get_total_orders() > 0
+            assert page.get_today_orders() > 0
+
+        with allure.step("Проверяем, что заказ появился в списке 'В работе'"):
+            order_number = page.get_order_number()
+            assert page.is_order_in_progress(order_number)
