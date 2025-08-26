905f2231c3e83fc2d2b10672ac062c946589f47c
krezzo1209 <kiriniki999@gmail.com>
2025-08-26 18:10:01 +0300

    test_login_user.py

--- API_tests/tests/test_login_user.py
+++ API_tests/tests/test_login_user.py
@@ -1,47 +1,34 @@
-import pytest
 import requests
 import allure
-from helpers import generate_user_data, register_user, delete_user
+from tests.data import ERROR_MESSAGES
 
 
-@allure.suite("Тесты API: Авторизация")
-@allure.feature("Вход пользователя")
+@allure.suite("API: Авторизация")
 class TestLoginUser:
 
     base_url = "https://stellarburgers.nomoreparties.site/api/v2"
 
     @allure.title("Успешный вход с корректными данными")
-    def test_login_existing_user(self):
-        # Регистрируем и логиним
-        payload = generate_user_data()
-        register_user(payload)
-
+    def test_login_existing_user(self, new_user):
+        payload, _ = new_user
         response = requests.post(f"{self.base_url}/auth/login", json={
             "email": payload["email"],
             "password": payload["password"]
         })
 
-        # Удаляем пользователя
-        delete_user(response.json().get("accessToken"))
-
         assert response.status_code == 200
         assert "accessToken" in response.json()
 
     @allure.title("Ошибка входа с неверным паролем")
-    def test_login_with_wrong_password(self):
-        payload = generate_user_data()
-        register_user(payload)
-
+    def test_login_with_wrong_password(self, new_user):
+        payload, _ = new_user
         response = requests.post(f"{self.base_url}/auth/login", json={
             "email": payload["email"],
             "password": "wrongpass"
         })
 
-        delete_user(response.json().get("accessToken"))
-
         assert response.status_code == 401
-        assert response.json()["success"] is False
-        assert "или пароль" in response.json()["message"]
+        assert ERROR_MESSAGES["unauthorized"] in response.json()["message"]
 
     @allure.title("Ошибка входа с несуществующим email")
     def test_login_with_unknown_email(self):
@@ -51,5 +38,4 @@ class TestLoginUser:
         })
 
         assert response.status_code == 401
-        assert response.json()["success"] is False
-        assert "или пароль" in response.json()["message"]
+        assert ERROR_MESSAGES["unauthorized"] in response.json()["message"]
