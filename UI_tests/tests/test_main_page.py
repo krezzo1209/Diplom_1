bb18c1d9e05e8cdf1fc57e11615c0eb695449e07
krezzo1209 <kiriniki999@gmail.com>
2025-08-29 15:07:05 +0300

    test_main_page.py

--- UI_tests/tests/test_main_page.py
+++ UI_tests/tests/test_main_page.py
@@ -11,30 +11,30 @@ class TestMainPage:
         page.open()
         page.click_order_feed_link()
         page.click_constructor_link()
-        assert page.is_constructor_page_opened()
+        assert "constructor" in page.get_current_url()
 
     @allure.title("Переход в Ленту заказов")
     def test_click_order_feed_link(self, driver):
         page = MainPage(driver)
         page.open()
         page.click_order_feed_link()
-        assert page.is_order_feed_page_opened()
+        assert "feed" in page.get_current_url()
 
     @allure.title("Открытие модального окна ингредиента")
     def test_click_ingredient_opens_modal(self, driver):
         page = MainPage(driver)
         page.open()
         page.click_first_ingredient()
-        assert page.is_modal_opened()
+        assert page.is_modal_visible()
 
     @allure.title("Закрытие модального окна по крестику")
     def test_close_modal_with_cross(self, driver):
         page = MainPage(driver)
         page.open()
         page.click_first_ingredient()
-        assert page.is_modal_opened()
+        assert page.is_modal_visible()
         page.close_modal()
-        assert page.is_modal_closed()
+        page.wait_for_modal_close()
 
     @allure.title("Счётчик увеличивается при добавлении ингредиента")
     def test_counter_increases_when_adding_ingredient(self, driver):
