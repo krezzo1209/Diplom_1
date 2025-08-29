17aad034a34d8aaa01dcee64a9b6b6e5c8c04195
krezzo1209 <kiriniki999@gmail.com>
2025-08-29 14:59:11 +0300

    test_order_feed.py

--- UI_tests/tests/test_order_feed.py
+++ UI_tests/tests/test_order_feed.py
@@ -1,26 +1,46 @@
 import allure
-from pages.main_page import MainPage
+from pages.order_feed_page import OrderFeedPage
 
 
 @allure.suite("UI: Лента заказов")
 class TestOrderFeed:
 
-    @allure.title("Счётчики заказов увеличиваются после оформления заказа")
-    def test_order_counters_increase(self, driver):
-        page = MainPage(driver)
+    @allure.title("Проверка, что страница Ленты заказов открывается")
+    def test_order_feed_page_opens(self, driver):
+        page = OrderFeedPage(driver)
         page.open()
+        assert page.is_order_feed_page_opened()
 
-        with allure.step("Добавляем ингредиент и оформляем заказ"):
-            page.click_first_ingredient()
-            page.place_order()
+    @allure.title("Проверка отображения заказов в ленте")
+    def test_orders_are_displayed_in_feed(self, driver):
+        page = OrderFeedPage(driver)
+        page.open()
+        assert page.is_orders_list_visible()
 
-        with allure.step("Ждём закрытия модального окна заказа"):
-            page.wait_for_invisibility(page.ORDER_NUMBER)
+    @allure.title("Проверка отображения счётчика 'Выполнено за всё время'")
+    def test_total_orders_counter_displayed(self, driver):
+        page = OrderFeedPage(driver)
+        page.open()
+        assert page.is_total_orders_counter_visible()
 
-        with allure.step("Проверяем, что счётчики увеличились"):
-            assert page.get_total_orders() > 0
-            assert page.get_today_orders() > 0
+    @allure.title("Проверка отображения счётчика 'Выполнено за сегодня'")
+    def test_today_orders_counter_displayed(self, driver):
+        page = OrderFeedPage(driver)
+        page.open()
+        assert page.is_today_orders_counter_visible()
 
-        with allure.step("Проверяем, что заказ появился в списке 'В работе'"):
-            order_number = page.get_order_number()
-            assert page.is_order_in_progress(order_number)
+    @allure.title("Клик по заказу открывает модальное окно")
+    def test_click_order_opens_modal(self, driver):
+        page = OrderFeedPage(driver)
+        page.open()
+        page.click_first_order()
+        assert page.is_order_modal_opened()
+
+    @allure.title("Закрытие модального окна заказа по крестику")
+    def test_close_order_modal_with_cross(self, driver):
+        page = OrderFeedPage(driver)
+        page.open()
+        page.click_first_order()
+        assert page.is_order_modal_opened()
+        page.close_order_modal()
+        assert page.is_order_modal_closed()
