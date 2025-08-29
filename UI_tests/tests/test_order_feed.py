4ae6bb20033e2d12fe81ffdd53c30ebaac5454a4
krezzo1209 <kiriniki999@gmail.com>
2025-08-29 15:08:08 +0300

    test_order_feed.py

--- UI_tests/tests/test_order_feed.py
+++ UI_tests/tests/test_order_feed.py
@@ -5,42 +5,32 @@ from pages.order_feed_page import OrderFeedPage
 @allure.suite("UI: Лента заказов")
 class TestOrderFeed:
 
-    @allure.title("Проверка, что страница Ленты заказов открывается")
-    def test_order_feed_page_opens(self, driver):
+    @allure.title("Переход в Конструктор из Ленты заказов")
+    def test_click_constructor_link_from_feed(self, driver):
         page = OrderFeedPage(driver)
         page.open()
-        assert page.is_order_feed_page_opened()
+        page.click_constructor_link()
+        assert "constructor" in page.get_current_url()
 
-    @allure.title("Проверка отображения заказов в ленте")
-    def test_orders_are_displayed_in_feed(self, driver):
-        page = OrderFeedPage(driver)
-        page.open()
-        assert page.is_orders_list_visible()
-
-    @allure.title("Проверка отображения счётчика 'Выполнено за всё время'")
-    def test_total_orders_counter_displayed(self, driver):
-        page = OrderFeedPage(driver)
-        page.open()
-        assert page.is_total_orders_counter_visible()
-
-    @allure.title("Проверка отображения счётчика 'Выполнено за сегодня'")
-    def test_today_orders_counter_displayed(self, driver):
-        page = OrderFeedPage(driver)
-        page.open()
-        assert page.is_today_orders_counter_visible()
-
-    @allure.title("Клик по заказу открывает модальное окно")
+    @allure.title("Открытие модального окна заказа")
     def test_click_order_opens_modal(self, driver):
         page = OrderFeedPage(driver)
         page.open()
         page.click_first_order()
-        assert page.is_order_modal_opened()
+        assert page.is_order_modal_visible()
 
     @allure.title("Закрытие модального окна заказа по крестику")
     def test_close_order_modal_with_cross(self, driver):
         page = OrderFeedPage(driver)
         page.open()
         page.click_first_order()
-        assert page.is_order_modal_opened()
-        page.close_order_modal()
-        assert page.is_order_modal_closed()
+        assert page.is_order_modal_visible()
+        page.close_modal()
+        page.wait_for_modal_close()
+
+    @allure.title("Проверка отображения номера заказа в ленте")
+    def test_order_number_displayed_in_feed(self, driver):
+        page = OrderFeedPage(driver)
+        page.open()
+        order_number = page.get_first_order_number()
+        assert order_number.isdigit(), "Номер заказа должен состоять из цифр"
