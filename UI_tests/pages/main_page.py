341adbf16712ac7fff88cb6affeb279411539376
krezzo1209 <kiriniki999@gmail.com>
2025-08-29 15:05:07 +0300

    main_page.py

--- UI_tests/pages/main_page.py
+++ UI_tests/pages/main_page.py
@@ -1,54 +1,46 @@
-# pages/main_page.py
-from .base_page import BasePage
+import allure
 from selenium.webdriver.common.by import By
+from pages.base_page import BasePage
 
 
 class MainPage(BasePage):
+    # Локаторы
+    ORDER_FEED_LINK = (By.LINK_TEXT, "Лента Заказов")
     CONSTRUCTOR_LINK = (By.LINK_TEXT, "Конструктор")
-    ORDER_FEED_LINK = (By.LINK_TEXT, "Лента заказов")
-    FIRST_INGREDIENT = (By.XPATH, "//p[text()='Флюоресцентная булка R2-D3']/ancestor::div[@class='card_root__LJvg2']")
-    MODAL_CLOSE_BUTTON = (By.CLASS_NAME, "Modal_modal__close__TnseK")
-    MODAL_TITLE = (By.XPATH, "//h2[text()='Детали ингредиента']")
-    BUN_COUNTER = (By.XPATH, "//p[text()='Флюоресцентная булка R2-D3']/preceding-sibling::p[@class='counter_counter__num__3CPl3']")
-    ORDER_BUTTON = (By.XPATH, "//button[text()='Оформить заказ']")
-    ORDER_NUMBER = (By.CLASS_NAME, "order-number")
-    TOTAL_ORDERS = (By.XPATH, "//p[text()='Выполнено за всё время']/following-sibling::p")
-    TODAY_ORDERS = (By.XPATH, "//p[text()='Выполнено за сегодня']/following-sibling::p")
-    IN_PROGRESS_ORDERS = (By.CSS_SELECTOR, ".OrderHistory_text__2M82Z")
+    FIRST_INGREDIENT = (By.CSS_SELECTOR, ".BurgerIngredient_ingredient__text__3eUdz")
+    MODAL_TITLE = (By.CSS_SELECTOR, ".Modal_modal__title__2s4mU")
+    CLOSE_MODAL_BUTTON = (By.CSS_SELECTOR, ".Modal_modal__close__2zc1h")
+    BUN_COUNTER = (By.CSS_SELECTOR, ".counter_counter__num__3nue1")
+    PLACE_ORDER_BUTTON = (By.XPATH, "//button[contains(text(),'Оформить заказ')]")
 
-    def open(self):
-        self.driver.get("https://stellarburgers.nomoreparties.site")
-
-    def click_constructor_link(self):
-        self.click_element(self.CONSTRUCTOR_LINK)
-
+    @allure.step("Переходим в ленту заказов")
     def click_order_feed_link(self):
         self.click_element(self.ORDER_FEED_LINK)
 
+    @allure.step("Переходим в конструктор")
+    def click_constructor_link(self):
+        self.click_element(self.CONSTRUCTOR_LINK)
+
+    @allure.step("Кликаем по первому ингредиенту")
     def click_first_ingredient(self):
         self.click_element(self.FIRST_INGREDIENT)
 
+    @allure.step("Проверяем, открыто ли модальное окно")
     def is_modal_visible(self):
         return self.is_visible(self.MODAL_TITLE)
 
+    @allure.step("Закрываем модальное окно")
     def close_modal(self):
-        self.click_element(self.MODAL_CLOSE_BUTTON)
+        self.click_element(self.CLOSE_MODAL_BUTTON)
 
+    @allure.step("Ожидаем, что модальное окно закроется")
+    def wait_for_modal_close(self):
+        return self.wait_for_invisibility(self.MODAL_TITLE)
+
+    @allure.step("Получаем значение счётчика булки")
     def get_bun_counter(self):
         return int(self.get_text(self.BUN_COUNTER))
 
+    @allure.step("Оформляем заказ")
     def place_order(self):
-        self.click_element(self.ORDER_BUTTON)
-
-    def get_order_number(self):
-        return int(self.get_text(self.ORDER_NUMBER))
-
-    def get_total_orders(self):
-        return int(self.get_text(self.TOTAL_ORDERS))
-
-    def get_today_orders(self):
-        return int(self.get_text(self.TODAY_ORDERS))
-
-    def is_order_in_progress(self, number):
-        orders = self.driver.find_elements(*self.IN_PROGRESS_ORDERS)
-        return str(number) in [order.text for order in orders]
+        self.click_element(self.PLACE_ORDER_BUTTON)
